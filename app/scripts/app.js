54009e16ca5683d669664c7a1a1ae3a904228bdd
Jack <jdcorley@gmail.com>
2017-10-11 19:16:59 -0400

    "assigment 3 completed"

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,24 @@
 (function () {
-  function config($stateProvider, $locationProvider){
+  function config($stateProvider, $locationProvider) {
     $locationProvider
       .html5Mode({
         enabled: true,
         requireBase: false
-      })
+      });
       $stateProvider
         .state('landing', {
-          url: '/'
+          url: '/',
           templateUrl: '/templates/landing.html'
         })
         .state('album', {
-          url: '/album'
+          url: '/album',
           templateUrl: '/templates/album.html'
-        });
+        })
+          .state('collection', {
+            url: '/collection',
+            templateUrl: '/templates/collection.html'
+
+    });
 }
 
 angular
