3f60502d3bb0a918cfb846a05aa0520cb1a67c65
Jack <jdcorley@gmail.com>
2017-10-24 08:01:00 -0400

    css corrections

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -12,24 +12,28 @@
       templateUrl: '/templates/directives/seek_bar.html',
       replace: true,
       restrict: 'E',
-      scope: {
-        onChange: '&'
-      },
+      scope: {},
       link: function(scope, element, attributes) {
         scope.value = 0;
         scope.max = 100;
+
         var seekBar = $(element);
+
         var percentString = function() {
           var value = scope.value;
           var max = scope.max;
           var percent = value / max * 100;
           return percent + "%";
         };
+
         scope.fillStyle = function() {
-          return {
-            width: percentString()
-          };
+          return {width: percentString()};
         };
+
+        scope.thumbStyle = function(){
+          return {width: percentString()};
+        };
+
         scope.onClickSeekBar = function(event) {
           var percent = calculatePercent(seekBar, event);
           scope.value = percent * scope.max;
@@ -38,13 +42,15 @@
           $document.bind('mousemove.thumb', function(event) {
             var percent = calculatePercent(seekBar, event);
             scope.$apply(function() {
-              scope.value = percent * scope.max;
+            scope.value = percent * scope.max;
             });
           });
+
           $document.bind('mouseup.thumb', function() {
             $document.unbind('mousemove.thumb');
             $document.unbind('mouseup.thumb');
           });
+
         };
       }
     };
